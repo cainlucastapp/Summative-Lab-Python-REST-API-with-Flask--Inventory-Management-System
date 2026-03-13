ec9129c7236c7f17363b5ab0e5a77bb55a416fde
cainlucastapp <cainlucastapp@agentformula.com>
2026-03-13 10:29:35 -0700

    moved templates ans static into the client level

---
+++ client/static/app.js
@@ -0,0 +1,19 @@
+// static/app.js
+
+// GET all inventory items
+
+// GET single inventory item
+
+// POST new inventory item
+
+// PATCH existing inventory item
+
+// DELETE inventory item
+
+// GET inventory count
+
+// GET search inventory
+
+// GET lookup product on OpenFoodFacts (by product name or barcode)
+
+// POST import product from OpenFoodFacts (by product name or barcode)
